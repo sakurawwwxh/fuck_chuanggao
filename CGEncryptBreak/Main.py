c1c92e03ccb8edc7e71500ee88e685963cda1357
nanguaguag <2022181640245@stu.scu.edu.cn>
2024-03-11 12:26:00 +0800

    修复bug

--- CGEncryptBreak/Main.py
+++ CGEncryptBreak/Main.py
@@ -1,22 +1,29 @@
 # -*- coding: utf-8 -*-
 import HttpReq
-from DataManager import check_is_valid
+import DataManager
 from GenerateData import generate_jsonsport
 
 
 def main():
-    user_info = HttpReq.login()
-    print("开始运动!")
-    prejudge_jsonsports, jsonsports = generate_jsonsport(None)
-    prejudge_resp = HttpReq.prejudge_req_new(
-        prejudge_jsonsports, user_info['token'], user_info['secret'])
-    print(prejudge_resp)
-    if check_is_valid(prejudge_resp):
-        print("prejudge 检验成功, 上传数据...")
-        if input("确定上传吗？y:上传;n:取消：") == "y":
-            savesports_resp = HttpReq.savesports_req(
-                jsonsports, user_info['token'], user_info['secret'])
-            print(savesports_resp)
+    while True:
+        user_info = HttpReq.login()
+        print("开始运动!")
+        prejudge_jsonsports, jsonsports = generate_jsonsport(None)
+        prejudge_resp = HttpReq.prejudge_req_new(
+            prejudge_jsonsports, user_info['token'], user_info['secret'])
+        print(prejudge_resp)
+        if DataManager.check_is_valid(prejudge_resp):
+            print("prejudge 检验成功, 上传数据...")
+            if input("确定上传吗？y:上传;n:取消：") == "y":
+                savesports_resp = HttpReq.savesports_req(
+                    jsonsports, user_info['token'], user_info['secret'])
+                print(savesports_resp)
+                return savesports_resp
+        elif prejudge_resp['code'] == 2001:
+            # 登录过期，请重新登录
+            print("登录已过期，正在重新登录")
+            DataManager.save_user_info({})
+            continue
 
 
 if __name__ == '__main__':
